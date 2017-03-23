9894bb8d6d301ee0b214db671d667505fd7e1eec
vichy60 <y.cappelle@orange.fr>
2017-03-23 13:29:28 +0100

    calculvoyelleconsonne

---
+++ exercices/comptage_voyelles_consonnes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace comptage_voyelles_consonnes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int voyelle;
+            int consonne;
+            Console.WriteLine("saisir un mot");
+            string mot = Console.ReadLine();
+            CalculVoyelleConsonne( mot,out voyelle, out consonne);
+            Console.WriteLine("\"{0}\" comprend {1} consonnes et {2} voyelles ", mot, consonne, voyelle);
+            Console.ReadKey();
+
+
+        }
+        static void CalculVoyelleConsonne(string saisie,out int nbv,out int nbc)
+        {
+            nbv = 0;
+            saisie = saisie.ToLower();
+            for (int i = 0; i < saisie.Length; i++)
+            {
+                if ((saisie[i] == 'a') || (saisie[i] == 'e')|| (saisie[i] == 'i')||(saisie[i] == 'o')|| (saisie[i] == 'u')|| (saisie[i] == 'y'))
+                {
+                    nbv++;
+                }
+            }
+
+            nbc =saisie.Length-nbv;
+
+            
+
+
+        }
+    }
+}
