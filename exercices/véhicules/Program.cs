74bfaef9adb3d43a33babb22231fad0d3fc1a853
vichy60 <y.cappelle@orange.fr>
2017-03-28 18:47:59 +0200

    vehicules

---
+++ exercices/véhicules/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace véhicules
+{
+
+    public enum Energies
+    {
+        Aucune,
+        Essence,
+        Gazole,
+        GPL,
+        Electrique
+    }
+    public class Véhicule
+    {
+        #region propriétés automatiques
+        public string Nom { get; }
+        public int NbRoues { get; }
+
+        public Energies Energie { get; }
+
+        public virtual string Description { get { return string.Format("Véhicule {0} roule sur {1} roues et à l'énergie {2}",Nom,NbRoues,Energie); } }
+          
+        #endregion
+
+        #region constructeur
+
+        public Véhicule(string nom, int nbRoues, Energies energie)
+        {
+            Nom = nom;
+            NbRoues = nbRoues;
+            Energie = energie;
+        }
+    }
+       public class Voiture : Véhicule
+        {
+            public Voiture(string nom, Energies energie) :base( nom, 4, energie)
+            {
+                
+            }
+
+
+        }
+
+        #endregion
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Voiture voiture1 = new Voiture("Clio", Energies.Essence);
+        }
+    }
+}
