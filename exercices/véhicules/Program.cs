1ff99dbecc40f921dd6597517662d5c1157cefde
vichy60 <y.cappelle@orange.fr>
2017-03-29 21:44:54 +0200

    recup du 29/03

--- exercices/véhicules/Program.cs
+++ exercices/véhicules/Program.cs
@@ -23,8 +23,8 @@ namespace véhicules
 
         public Energies Energie { get; }
 
-        public virtual string Description { get { return string.Format("Véhicule {0} roule sur {1} roues et à l'énergie {2}",Nom,NbRoues,Energie); } }
-          
+        public virtual string Description { get { return string.Format("Véhicule {0} roule sur {1} roues et à l'énergie {2}", Nom, NbRoues, Energie); } }
+
         #endregion
 
         #region constructeur
@@ -36,28 +36,45 @@ namespace véhicules
             Energie = energie;
         }
     }
-       public class Voiture : Véhicule
-        {
+    public class Voiture : Véhicule
+    {
         public override string Description
-        { get { return string.Format("Je suis une voiture \r\n " + base.Description);}        }
+        { get { return string.Format("Je suis une voiture \r\n " + base.Description); } }
 
-        public Voiture(string nom, Energies energie) :base( nom, 4, energie)
-            {
-                
-            }
+        public Voiture(string nom, Energies energie) : base(nom, 4, energie)
+        {
 
+        }
+
+
+    }
+
+    public class Moto : Véhicule
+    {
+        public override string Description
+        { get { return string.Format("Je suis une moto \r\n " + base.Description); } }
+
+        public Moto(string nom, Energies energie) : base(nom, 2, energie)
+        {
 
         }
 
         #endregion
-    
-    class Program
-    {
-        static void Main(string[] args)
+
+        class Program
         {
-            Voiture voiture1 = new Voiture("Clio", Energies.Essence);
-            Console.WriteLine(voiture1.Description);
-            Console.ReadKey();
+            static void Main(string[] args)
+            {
+                Voiture voiture1 = new Voiture("Clio", Energies.Essence);
+                Console.WriteLine(voiture1.Description);
+
+
+                Véhicule voiture2 = new Voiture("Peugeot", Energies.Gazole);
+                Console.WriteLine(voiture2.Description);
+                Véhicule Moto2 = new Moto("suzuki", Energies.Electrique);
+                Console.WriteLine(Moto2.Description);
+                Console.ReadKey();
+            }
         }
     }
 }
