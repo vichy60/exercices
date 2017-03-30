01f5067f328a0f10a35be33c3f461dcaa6f62412
vichy60 <y.cappelle@orange.fr>
2017-03-30 19:44:35 +0200

    exercice florian 30/03

---
+++ exercices/exercice_Florian/Ancetre.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice_Florian
+{
+    public abstract class Ancetre : IMangeable
+    {
+        #region MyRegion
+
+        public virtual string Description { get; }
+        public static int Compteur { get; private set; }
+
+        #endregion
+
+        #region Constructeur
+        public Ancetre()
+        {
+            Description = "je suis l'ancètre";
+            Compteur++;
+            
+        }
+
+        public virtual bool Mangeable()
+        {
+            return true;
+           
+        }
+
+        #endregion
+    }
+}
---
+++ exercices/exercice_Florian/fils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice_Florian
+{
+    class fils : Ancetre
+    {
+        #region propriété automatique
+        public override string Description { get; }
+
+        #endregion
+
+        #region constructeur
+
+        public fils() : base()
+        {
+            Description = "je suis un fils";
+        }
+        #endregion
+    }
+
+
+    class fille : Ancetre
+    {
+        #region proprièté automatique
+
+        public override string Description { get; }
+        #endregion
+
+        #region constructeur
+        public fille() : base()
+        {
+            Description = "je suis une fille";
+        }
+
+        #endregion
+
+        #region methode publique
+        public override bool Mangeable()
+        {
+            return false;
+        }
+
+        #endregion
+    }
+}
